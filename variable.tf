205c7bceb0f4e775ae08b6b5dead705e7565f3cf
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-03-07 12:05:35 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,31 @@
+variable "tfResourceGroup" {
+
+  type = string
+
+}
+
+variable "tfEnvironmentKey"{
+
+  type = string
+
+  default="devaks"
+
+}
+
+variable "tfCosmosDb" {
+
+  type = string
+
+}
+
+variable "tfCosmosAC" {
+
+  type = string
+
+}
+
+variable "tfRgLocation" {
+
+  type = string
+
+}
